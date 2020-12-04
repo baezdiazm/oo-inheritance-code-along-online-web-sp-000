0183157c6cb1a900c9dee50e14afcfd902176922
Manuel Eduardo Baez Diaz <baezdiazm@yahoo.com>
2020-12-04 04:35:15 +0000

    Automatically backed up by Learn

--- lib/car.rb
+++ lib/car.rb
@@ -3,8 +3,9 @@ require_relative "./vehicle.rb"
 class Car
   attr_accessor :wheel_size, :wheel_number
 
-  def initialize()
-
+  def initialize(wheel_size, wheel_number)
+    @Wheel_size = wheel_size
+    @wheel_number = wheel_number
   end
 
 end
